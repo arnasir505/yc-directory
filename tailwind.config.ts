a2dd958041796827fcd44f9c0f0953cdc9f1a203
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-11-18 11:49:48 +0600

    Install fonts, create hero section, add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,7 +45,6 @@ const config: Config = {
       },
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
 
